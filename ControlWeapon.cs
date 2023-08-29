a251da049e9eff1ccfadcf8cdb6d5359ab911777
Danillo <danillo.damian@hotmail.com>
2023-08-29 15:06:23 -0300

    Solved the performance of ammunition generation code

--- ControlWeapon.cs
+++ ControlWeapon.cs
@@ -7,12 +7,57 @@ public class ControlWeapon : MonoBehaviour
     public GameObject Ammunition;
     public GameObject GunBarrel;
     public AudioClip GunshotSound;
+    private int initialNNumberList = 20;
+    public List<Ammunition> listAmmunition;
     
+    void Start()
+    {
+        InitListAmmunition();
+    }
+
     void Update()
     {
         if(Input.GetButtonDown("Fire1")){
-            Instantiate(Ammunition, GunBarrel.transform.position, GunBarrel.transform.rotation);
+            StartCoroutine(GenerateAmmunition(GunBarrel.transform.position, GunBarrel.transform.rotation));
             ControlAudio.instance.PlayOneShot(GunshotSound);
         }        
     }
+
+    void InitListAmmunition()
+    {
+        for(int i=0;i<initialNNumberList;i++)
+        {
+            InitAmmunition();
+        }
+
+    }
+    void InitAmmunition()
+    {
+        GameObject obj = Instantiate(Ammunition, transform.position, transform.rotation);
+        obj.SetActive(false);
+        listAmmunition.Add(obj.GetComponent<Ammunition>());
+    }
+
+    IEnumerator GenerateAmmunition(Vector3 positionStart,Quaternion rotationStart)
+    {
+        yield return null;
+        int i = Random.Range(0,listAmmunition.Count);
+        while(true){
+            var ammunition = listAmmunition[i];            
+            if(!ammunition.gameObject.activeInHierarchy)
+            {
+                ammunition.gameObject.SetActive(true);                
+                ammunition.transform.position = positionStart;    
+                ammunition.transform.rotation = rotationStart;    
+
+                ammunition.GetComponent<Collider>().enabled = true;
+                break;
+            }else{
+                i = Random.Range(0,listAmmunition.Count);
+            }
+        }
+
+    }
+
+
 }
